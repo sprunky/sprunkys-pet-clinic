d3c80ade031f41036d084c4fc6582b7c3cd1ac25
jonathan.garrido <jonathan.garrido@incentro.com>
2021-11-08 15:20:31 +0100

    Initial commit

---
+++ src/main/java/com/sprunky/sprunkyspetclinic/SprunkysPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.sprunky.sprunkyspetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprunkysPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprunkysPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sprunky/sprunkyspetclinic/SprunkysPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sprunky.sprunkyspetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprunkysPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
