02e460ff7965683cebd8563eabb424f9329d4103
jonathan.garrido <jonathan.garrido@incentro.com>
2021-11-30 13:50:42 +0100

    Added Owners list and updated controller. closes #16

--- pet-clinic-web/src/main/java/com/sprunky/sprunkyspetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/sprunky/sprunkyspetclinic/bootstrap/DataLoader.java
@@ -26,9 +26,9 @@ public class DataLoader implements CommandLineRunner {
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner1.setId(2L);
-        owner1.setFirstName("Fiona");
-        owner1.setLastName("Gleanne");
+        owner2.setId(2L);
+        owner2.setFirstName("Fiona");
+        owner2.setLastName("Gleanne");
         ownerService.save(owner2);
 
         System.out.println("Loaded Owners.....");
